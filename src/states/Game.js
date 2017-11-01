bb6abd1e92aa9dd365445564b9996bccad2fe141
Val Saven <val.saven@gmail.com>
2017-11-01 11:34:29 +0300

    Add flying control and shooting

--- src/states/Game.js
+++ src/states/Game.js
@@ -4,7 +4,10 @@ import Mushroom from '../sprites/Mushroom'
 
 export default class extends Phaser.State {
   init () {}
-  preload () {}
+  preload () {
+    this.game.load.image('bulletPlayer', './assets/images/player-bullet.png')
+    this.game.load.image('bulletEnemy', './assets/images/enemy-bullet.png')
+  }
 
   create () {
     const bannerText = 'Phaser + ES6 + Webpack'
@@ -25,62 +28,68 @@ export default class extends Phaser.State {
     })
 
     this.game.add.existing(this.mushroom)
-  }
-
-  update() {
-    const player = this.mushroom
 
     // Input
-    const upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.UP)
-    const downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN)
-    const leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
-    const rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
+    this.upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.UP)
+    this.downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN)
+    this.leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
+    this.rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
 
     // Focusing
-    const shiftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SHIFT)
+    this.focusKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SHIFT)
     // Shoot
-    const zKey = this.game.input.keyboard.addKey(Phaser.Keyboard.Z)
+    this.fireKey = this.game.input.keyboard.addKey(Phaser.Keyboard.Z)
     // Bomb
-    const xKey = this.game.input.keyboard.addKey(Phaser.Keyboard.X)
+    this.bombKey = this.game.input.keyboard.addKey(Phaser.Keyboard.X)
 
     // Speed
-    const speed = 4
-    const playerSpeed = shiftKey.isDown ? speed / 2 : speed
+    this.speed = 4
 
-    if (upKey.isDown) {
+    //  Creates 30 bullets, using the 'bullet' graphic
+    this.weapon = this.game.add.weapon(30, 'bulletPlayer')
+
+    //  The bullet will be automatically killed when it leaves the world bounds
+    this.weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS
+
+    //  The speed at which the bullet is fired
+    this.weapon.bulletSpeed = 600
+
+    //  Speed-up the rate of fire, allowing them to shoot 1 bullet every 60ms
+    this.weapon.fireRate = 100
+
+    //  Tell the Weapon to track the 'player' Sprite
+    //  With no offsets from the position
+    //  But the 'true' argument tells the weapon to track sprite rotation
+    this.weapon.trackSprite(this.mushroom, 0, 0, false)
+  }
+
+  update () {
+    const player = this.mushroom
+    const playerSpeed = this.focusKey.isDown ? this.speed / 2 : this.speed
+
+    if (this.fireKey.isDown) {
+      this.weapon.fire()
+    }
+
+    if (this.bombKey.isDown) {
+      console.log('Boom!')
+    }
+
+    if (this.upKey.isDown) {
       player.y -= playerSpeed
-    } else if (downKey.isDown) {
+    } else if (this.downKey.isDown) {
       player.y += playerSpeed
     }
 
-    if (leftKey.isDown) {
+    if (this.leftKey.isDown) {
       player.x -= playerSpeed
       player.angle = -15
-    } else if (rightKey.isDown) {
+    } else if (this.rightKey.isDown) {
       player.x += playerSpeed
       player.angle = +15
     } else {
       player.rotation = 0
     }
-
-    // if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-    //   this.mushroom.x -= speed
-    //   this.mushroom.angle = -15
-    //   // leftBtn.alpha = 0.6
-    // } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-    //   this.mushroom.x += speed
-    //   this.mushroom.angle = 15
-    //   // rightBtn.alpha = 0.6
-    // } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-    //   this.mushroom.y -= speed
-    //   console.log('up')
-    // } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-    //   this.mushroom.y += speed
-    //   console.log('down')
-    // } else {
-    //   this.mushroom.rotation = 0
-    //   // leftBtn.alpha = rightBtn.alpha = 0
-    // }
   }
 
   render () {
