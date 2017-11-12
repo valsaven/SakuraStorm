d52ce0ac40898da212ed6149b07581509e160047
Val Saven <val.saven@gmail.com>
2017-11-12 03:26:48 +0300

    Refactor player input
    Move it to separate functions

--- src/states/Game.js
+++ src/states/Game.js
@@ -7,10 +7,10 @@ export default class extends Phaser.State {
   preload () {
     this.game.load.image('bulletPlayer', './assets/images/player-bullet.png')
     this.game.load.image('bulletEnemy', './assets/images/enemy-bullet.png')
+    this.game.load.image('enemy', './assets/images/enemy.png')
   }
-
   create () {
-    const bannerText = 'Phaser + ES6 + Webpack'
+    const bannerText = 'SakuraStorm'
     let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
 
     banner.font = 'Bangers'
@@ -29,17 +29,10 @@ export default class extends Phaser.State {
 
     this.game.add.existing(this.player)
 
-    // Arrows
-    this.cursors = this.game.input.keyboard.createCursorKeys()
-    // Focusing
-    this.focusKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SHIFT)
-    // Fire
-    this.fireKey = this.game.input.keyboard.addKey(Phaser.Keyboard.Z)
-    // Bomb
-    this.bombKey = this.game.input.keyboard.addKey(Phaser.Keyboard.X)
+    this.player.input = new PlayerInput(Phaser, this.game)
 
     // Speed
-    this.speed = 4
+    this.player.speed = 4
 
     //  Creates 30 bullets, using the 'bullet' graphic
     this.weapon = this.game.add.weapon(30, 'bulletPlayer')
@@ -57,39 +50,83 @@ export default class extends Phaser.State {
     //  With no offsets from the position
     //  But the 'true' argument tells the weapon to track sprite rotation
     this.weapon.trackSprite(this.player, 0, 0, false)
-  }
 
+    var Enemy = function (game, x, y) {
+      Phaser.Sprite.call(this, game, x, y, 'enemy')
+    }
+    Enemy.prototype = Object.create(Phaser.Sprite.prototype)
+    Enemy.prototype.constructor = Enemy
+
+    // Level 1
+    setTimeout(function () {
+      console.log(123)
+      this.enemy = new Enemy(this.game, 200, 300)
+      this.game.add.existing(this.enemy)
+    }, 1000)
+  }
   update () {
-    const playerSpeed = this.focusKey.isDown ? this.speed / 2 : this.speed
-
-    if (this.fireKey.isDown) {
-      this.weapon.fire()
-    }
-
-    if (this.bombKey.isDown) {
-      console.log('Boom!')
-    }
-
-    if (this.cursors.up.isDown) {
-      this.player.y -= playerSpeed
-    } else if (this.cursors.down.isDown) {
-      this.player.y += playerSpeed
-    }
-
-    if (this.cursors.left.isDown) {
-      this.player.x -= playerSpeed
-      this.player.angle = -15
-    } else if (this.cursors.right.isDown) {
-      this.player.x += playerSpeed
-      this.player.angle = +15
-    } else {
-      this.player.rotation = 0
-    }
+    listenPlayerInput.call(this, this.player.input)
   }
-
   render () {
     if (__DEV__) {
       this.game.debug.spriteInfo(this.player, 32, 32)
     }
+  }
+}
+
+/**
+ PlayerInput - Creates object with player's input keys.
+
+ @class PlayerInput
+ @type {Object}
+
+ @param {Object} Phaser
+ @param {Object} game - this.game
+
+ @property {Object} cursors - Arrows.
+ @property {Object} focusKey - Focusing.
+ @property {Object} fireKey - Fire.
+ @property {Object} bombKey - Bomb.
+ */
+function PlayerInput (Phaser, game) {
+  return {
+    cursors: game.input.keyboard.createCursorKeys(),
+    focusKey: game.input.keyboard.addKey(Phaser.Keyboard.SHIFT),
+    fireKey: game.input.keyboard.addKey(Phaser.Keyboard.Z),
+    bombKey: game.input.keyboard.addKey(Phaser.Keyboard.X)
+  }
+}
+
+/**
+ listenPlayerInput - listen player's input.
+ Should be called with 'this'.
+
+ @param {this.player.input} input - this.player.input
+ */
+function listenPlayerInput (input) {
+  const playerSpeed = input.focusKey.isDown ? this.player.speed / 2 : this.player.speed
+
+  if (input.fireKey.isDown) {
+    this.weapon.fire()
+  }
+
+  if (input.bombKey.isDown) {
+    console.log('Boom!')
+  }
+
+  if (input.cursors.up.isDown) {
+    this.player.y -= playerSpeed
+  } else if (input.cursors.down.isDown) {
+    this.player.y += playerSpeed
+  }
+
+  if (input.cursors.left.isDown) {
+    this.player.x -= playerSpeed
+    this.player.angle = -15
+  } else if (input.cursors.right.isDown) {
+    this.player.x += playerSpeed
+    this.player.angle = +15
+  } else {
+    this.player.rotation = 0
   }
 }
